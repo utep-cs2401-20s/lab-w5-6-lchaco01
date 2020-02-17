8296c1e9d3c0bfa74c515c1117cf72d7b48f0dd2
lchaco01 <Darklord00!>
2020-02-17 14:49:06 -0700

    added Snake game to rep added test to rep added constructors

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -1,2 +1,18 @@
 public class SnakeGame {
+    private boolean[][] game;
+    private int[] headPosition;
+    private static int exhaustiveChecks;
+    private static int recursiveChecks;
+
+    public SnakeGame() {
+        game = new boolean [1][1];
+    }
+
+    public SnakeGame(boolean[][] g) {
+        for (int i = 0; i < game.length; i++) {
+            for (int j = 0; i < game[i].length; j++) {
+                g[i][j] = game[i][j];
+            }
+        }
+    }
 }
