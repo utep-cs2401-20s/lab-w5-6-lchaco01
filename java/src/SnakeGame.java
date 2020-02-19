66ec17de6af244976b9b51d45bb6c243bd9d12a4
lchaco01 <Darklord00!>
2020-02-19 14:48:33 -0700

    added Snake game to rep added test to rep added constructors added new methods to check the cells and for the tail.

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -8,11 +8,45 @@ public class SnakeGame {
         game = new boolean [1][1];
     }
 
-    public SnakeGame(boolean[][] g) {
+    public SnakeGame(boolean[][] g, int x, int y) {
+        headPosition = new int [2];
+        headPosition[0] = x;
+        headPosition[1] = y;
+        game = g;
+    }
+
+    public int[] findTailExhaustive(int x, int y) {
+        int cells = 0;
+        int length = 0;
+        int[] result = new int [3];
         for (int i = 0; i < game.length; i++) {
-            for (int j = 0; i < game[i].length; j++) {
-                g[i][j] = game[i][j];
+            for (int j = 0; j < game[i].length; j++) {
+                int count;
+                if (game[i][j]) {
+                    length++;
+                    if () {
+
+                    }
+                }
+                cells++;
             }
         }
+        return result;
+    }
+
+    public int[] fineTailRecursive(int x, int y) {
+        int cells = 0;
+        int length = 0;
+        int[] result = new int [3];
+
+        return result;
+    }
+
+    private int[] findTailRecursive(int[] currentPosition, int[] previousPosition) {
+
+    }
+
+    private void resetChecks() {
+
     }
 }
